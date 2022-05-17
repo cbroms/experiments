ca3d62f0aa9dab54744609b139fa9780722053e7
Christian Broms <cb@christianbroms.com>
2022-05-17 08:17:21 -0700

    swap index pages

--- color-similarity/src/routes/index.svelte
+++ color-similarity/src/routes/index.svelte
@@ -1,199 +1,211 @@
-<script context="module">
-	export const prerender = true;
-</script>
-
 <script>
+	import Projection3D from '$lib/components/Projection3D.svelte';
+	import Projection2D from '$lib/components/Projection2D.svelte';
+
+	import { make2DOKLCHMap, makeLCHColor, generatePalette } from '$lib/colors.js';
+	import { setupCanvas, renderColorMap } from '$lib/canvas';
+	import { onMount } from 'svelte';
 	import { debounce } from 'lodash-es';
 
-	import {
-		getOpposite,
-		hexToLCH,
-		LCHToHex,
-		make2DCIELCHMap,
-		make2DOKLCHMap,
-		generateRandom,
-		generatePalette,
-		paletteToCvdPalette,
-		modifyColor,
-		generatePaletteIcosahedron
-	} from '$lib/colors.js';
-
-	import ColorMap from '$lib/components/ColorMap.svelte';
-	import DistanceMetrics from '$lib/components/DistanceMetrics.svelte';
+	import InputControl from '$lib/components/InputControl.svelte';
 	import Swatch from '$lib/components/Swatch.svelte';
-	import { onMount } from 'svelte';
+	import DistanceMetrics from '$lib/components/DistanceMetrics.svelte';
 
-	let inputColor = '#fff';
-	let colors = [];
-	let colorsCvd = [];
-
-	let chroma;
-	let lightness;
-	let hue;
-
+	let canvas;
 	let mapColors;
-	let mapColorsCvd;
 
-	let distance = { max: 0, min: 0, sum: 0 };
-	let distanceCvd = { ...distance };
+	let chroma = 0.25;
+	let lightness = 0.6;
+	let hue = 20;
 
-	let lightnessMax;
-	let lightnessMin;
+	let minLightness = 0.6;
+	let maxLightness = 0.8;
+	let numColors = 8;
 
-	let adjustForCvd = false;
+	let palette = { colors: [] };
+
+	let generatePaletteMode = false;
+	let project3D = true;
+
+	let radius = null;
+
+	const maxProjectionRadius = 2;
+	const minProjectionRadius = 0.2;
 
 	onMount(() => {
-		// lightnessMax = 85;
-		// lightnessMin = 50;
-		// chroma = 120;
-
-		lightnessMax = 0.9;
-		lightnessMin = 0.5;
-		chroma = 0.2;
-
-		inputColor = generateRandom(chroma, [lightnessMin, lightnessMax]);
-		// mapColors = make2DCIELCHMap(chroma, false);
-		// mapColorsCvd = make2DCIELCHMap(chroma, true);
-		mapColors = make2DOKLCHMap(chroma, false);
-		mapColorsCvd = make2DOKLCHMap(chroma, true);
-
-		updateValuesFromColor();
+		canvas = setupCanvas();
+		repaintMap();
 	});
 
-	const updateValuesFromColor = () => {
-		const newChoma = inputColor.c;
-		if (newChoma !== chroma) {
-			mapColors = make2DOKLCHMap(newChoma);
-			mapColorsCvd = make2DOKLCHMap(newChoma, true);
-		}
-		chroma = newChoma;
-		lightness = inputColor.l;
-		hue = inputColor.h;
-
-		updateColorPalette();
+	const chromaToRadius = (chroma) => {
+		return (
+			((chroma - 0) / (0.322 - 0)) * (maxProjectionRadius - minProjectionRadius) +
+			minProjectionRadius
+		);
 	};
 
-	const updateColorPalette = () => {
-		const newPalette = generatePalette(inputColor, 10, [lightnessMin, lightnessMax], adjustForCvd);
-		const newPaletteCvd = paletteToCvdPalette(newPalette);
-		// const newPalette = generatePaletteIcosahedron(chroma);
-		colors = newPalette.colors;
-		colorsCvd = newPaletteCvd.colors;
-		distance = newPalette.distance;
-		distanceCvd = newPaletteCvd.distance;
-	};
-
-	const onCvdCheck = (e) => {
-		if (e.target.checked) {
-			adjustForCvd = true;
-			updateColorPalette();
+	const repaintMap = debounce(() => {
+		const color = makeLCHColor({ l: lightness, c: chroma, h: hue });
+		if (!generatePaletteMode) {
+			palette = { colors: [color] };
 		} else {
-			adjustForCvd = false;
-			updateColorPalette();
+			// generate palette
+			palette = generatePalette(color.lch, numColors, [minLightness, maxLightness]);
 		}
-	};
-
-	const onChromaInput = (e) => {
-		inputColor = modifyColor(inputColor, { c: parseFloat(e.target.value) });
-		updateValuesFromColor();
-	};
-
-	const onHueInput = (e) => {
-		inputColor = modifyColor(inputColor, { h: parseFloat(e.target.value) });
-		updateValuesFromColor();
-	};
-
-	const onColorInput = debounce((e) => {
-		inputColor = hexToLCH(e.target.value);
-		updateValuesFromColor();
+		mapColors = make2DOKLCHMap(chroma);
+		canvas = renderColorMap(
+			canvas,
+			mapColors,
+			palette.colors,
+			generatePaletteMode ? [minLightness, maxLightness] : []
+		);
+		radius = chromaToRadius(chroma);
 	}, 300);
 
-	const onMinLightnessInput = (e) => {
-		lightnessMin = parseFloat(e.target.value);
-		updateValuesFromColor();
+	const onChromaChange = (e) => {
+		chroma = e.detail.value;
+		repaintMap();
 	};
 
-	const onMaxLightnessInput = (e) => {
-		lightnessMax = parseFloat(e.target.value);
-		updateValuesFromColor();
+	const onLightnessChange = (e) => {
+		lightness = e.detail.value;
+		repaintMap();
+	};
+
+	const onHueChange = (e) => {
+		hue = e.detail.value;
+		repaintMap();
+	};
+
+	const onMinLightnessChange = (e) => {
+		minLightness = e.detail.value;
+		repaintMap();
+	};
+
+	const onMaxLightnessChange = (e) => {
+		maxLightness = e.detail.value;
+		repaintMap();
+	};
+
+	const onGeneratePaletteChange = (e) => {
+		generatePaletteMode = e.target.checked;
+		repaintMap();
+	};
+
+	const onProject3DChange = (e) => {
+		project3D = e.target.checked;
+		repaintMap();
+	};
+
+	const onNumColorsChange = (e) => {
+		numColors = parseInt(e.detail.value);
+		repaintMap();
 	};
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<div>hello</div>
-	<input on:input={onColorInput} type="color" value={LCHToHex(inputColor)} />
-	<div>
-		<label for="chroma">Chroma</label>
-		<input
-			on:change={onChromaInput}
+<div class="map-wrapper">
+	<div class="map-controls">
+		<InputControl
+			name="Chroma (C*)"
+			min={0}
+			max={0.322}
 			value={chroma}
-			id="chroma"
-			type="range"
-			min="0"
-			max="0.322"
-			step="0.01"
+			step={0.01}
+			on:change={onChromaChange}
 		/>
-		<label for="hue">Hue</label>
-		<input on:change={onHueInput} value={hue} id="hue" type="range" min="0" max="360" />
-		<div>
-			<label for="min-lightness">Min</label>
+		<InputControl
+			name="Lightness (L*)"
+			min={0}
+			max={0.99}
+			value={lightness}
+			step={0.01}
+			on:change={onLightnessChange}
+		/>
+		<InputControl name="Hue (h°)" min={0} max={360} value={hue} step={1} on:change={onHueChange} />
 
-			<input
-				on:change={onMinLightnessInput}
-				value={lightnessMin}
-				id="min-lightness"
-				type="number"
-				min="0"
-				max="1"
-				step="0.01"
+		<input
+			id="generate"
+			type="checkbox"
+			checked={generatePaletteMode}
+			on:change={onGeneratePaletteChange}
+		/>
+		<label for="generate">Generate palette</label>
+
+		{#if generatePaletteMode}
+			<InputControl
+				name="Palette size"
+				min={2}
+				max={40}
+				value={numColors}
+				step={1}
+				on:change={onNumColorsChange}
+				slider={false}
 			/>
-			<label for="max-lightness">Max</label>
-			<input
-				on:change={onMaxLightnessInput}
-				value={lightnessMax}
-				id="max-lightness"
-				type="number"
-				min="0"
-				max="1"
-				step="0.01"
+			<InputControl
+				name="L* Min"
+				min={0}
+				max={0.99}
+				value={minLightness}
+				step={0.01}
+				on:change={onMinLightnessChange}
+				slider={false}
 			/>
+			<InputControl
+				name="L* Max"
+				min={0}
+				max={0.99}
+				value={maxLightness}
+				step={0.01}
+				on:change={onMaxLightnessChange}
+				slider={false}
+			/>
+		{/if}
+	</div>
+
+	<div class="map-content">
+		<!-- <div>
+			<input id="projection" type="checkbox" checked={project3D} on:change={onProject3DChange} />
+			<label for="projection">3D Projection</label>
+		</div> -->
+
+		<div class="map">
+			{#if project3D}
+				<Projection3D {canvas} {radius} maxRadius={maxProjectionRadius} />
+			{:else}
+				<Projection2D {canvas} />
+			{/if}
 		</div>
+		<div class="map-palette">
+			{#each palette.colors as color}
+				<Swatch hex={color.hex} />
+			{/each}
+		</div>
+		{#if generatePaletteMode && palette.distance}
+			<DistanceMetrics distance={palette.distance} />
+		{/if}
 	</div>
-	<div />
-
-	<input type="checkbox" on:change={onCvdCheck} />
-	<!-- <button on:click={randomColor}>Generate Random</button> -->
-
-	<div>
-		{#each colors as col}
-			<Swatch hex={col.hex} />
-		{/each}
-	</div>
-	<div>
-		<DistanceMetrics {distance} />
-	</div>
-	<div>
-		<ColorMap colors={mapColors} palette={colors} {lightnessMax} {lightnessMin} />
-	</div>
-
-	<hr />
-	<div>
-		{#each colorsCvd as col}
-			<Swatch hex={col.hex} />
-		{/each}
-	</div>
-	<div>
-		<DistanceMetrics distance={distanceCvd} />
-	</div>
-	<div>
-		<ColorMap colors={mapColorsCvd} palette={colorsCvd} {lightnessMax} {lightnessMin} />
-	</div>
-</section>
+</div>
 
 <style>
+	.map-wrapper {
+		display: flex;
+	}
+
+	.map-controls {
+		padding: 4rem;
+	}
+
+	.map {
+		width: 500px;
+		background-color: #f5f5f5;
+		display: flex;
+		justify-content: center;
+		border-radius: 12px;
+	}
+
+	.map-content {
+		max-width: 500px;
+		display: flex;
+		flex-flow: column;
+	}
 </style>
