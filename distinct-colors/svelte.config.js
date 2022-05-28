03ec162125327bc6b92e10c73b76a00967c5291c
Christian Broms <cb@christianbroms.com>
2022-05-27 19:10:13 -0700

    rename color experiment

---
+++ distinct-colors/svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({
+      precompress: true
+    }),
+    files: {
+      assets: 'static'
+    },
+    paths: {
+      base: dev ? '' : '/experiment/distinct-colors'
+    },
+    browser: {
+      router: false
+    }
+  }
+};
+
+export default config;
